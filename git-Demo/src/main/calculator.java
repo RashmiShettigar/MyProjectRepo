e2dab624d45cada922789dcc1c6446c3e73f8329
Rashmi <1rashmishettigar@gmail.com>
2021-03-23 09:53:13 +0530

    Mod and Div

--- git-Demo/src/main/calculator.java
+++ git-Demo/src/main/calculator.java
@@ -12,7 +12,6 @@ public class calculator {
 	public void diff(int num1, int num2){
 		System.out.println(num1-num2);
 	}
-	
 	public void mod(int num1, int num2){
 		System.out.println(num1%num2);
 	}
